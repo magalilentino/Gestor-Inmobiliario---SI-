ee0df868f88ac7faaa3b57174f5c3747ab056a40
Valeria <valelupopiatti@gmail.com>
2025-09-01 19:33:05 -0300

    first commit

---
+++ src/main/java/com/gestorInmobiliario/seminario/SeminarioApplication.java
@@ -0,0 +1,13 @@
+package com.gestorInmobiliario.seminario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeminarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeminarioApplication.class, args);
+	}
+
+}
