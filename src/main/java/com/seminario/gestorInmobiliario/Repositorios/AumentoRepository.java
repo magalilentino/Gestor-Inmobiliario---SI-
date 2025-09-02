7205589d0fea71e0f76de9e5f006691701a04acf
Gabriel Perlin <134007977+PeKa98@users.noreply.github.com>
2025-09-02 11:00:04 -0300

    Creacion de Repositorios y algunos serivicios y un controlador

---
+++ src/main/java/com/seminario/gestorInmobiliario/Repositorios/AumentoRepository.java
@@ -0,0 +1,7 @@
+package com.seminario.gestorInmobiliario.Repositorios;
+
+import com.seminario.gestorInmobiliario.Entidades.Aumento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AumentoRepository extends JpaRepository<Aumento, Long> {
+}
