02e1de9694e771c40b21f09079b138483e8857f3
Valeria <valelupopiatti@gmail.com>
2025-09-02 07:44:17 -0300

    create entidades

--- src/main/java/com/seminario/gestorInmobiliario/Entidades/Visitas.java
+++ src/main/java/com/seminario/gestorInmobiliario/Entidades/Visitas.java
@@ -2,15 +2,11 @@ package com.seminario.gestorInmobiliario.Entidades;
 
 
 import java.time.LocalDateTime;
-
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 
 @Entity
