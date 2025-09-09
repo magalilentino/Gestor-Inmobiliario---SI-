c42bae7d5996e247343629bdc759fd112d034d8e
Magali <magalilentino@gmail.com>
2025-09-08 22:19:34 -0300

    se agregó etiqueta repository

--- src/main/java/com/seminario/gestorInmobiliario/Repositorios/ProvinciaRepository.java
+++ src/main/java/com/seminario/gestorInmobiliario/Repositorios/ProvinciaRepository.java
@@ -2,6 +2,8 @@ package com.seminario.gestorInmobiliario.Repositorios;
 
 import com.seminario.gestorInmobiliario.Entidades.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProvinciaRepository extends JpaRepository<Provincia, Integer> {
 }
