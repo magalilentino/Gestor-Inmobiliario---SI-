1274a26f56e10e4517e4411d7cbc94fdb1f841e8
Magali <magalilentino@gmail.com>
2025-09-08 22:19:34 -0300

    se agregó etiqueta repository

--- src/main/java/com/seminario/gestorInmobiliario/Repositorios/AgenteRepository.java
+++ src/main/java/com/seminario/gestorInmobiliario/Repositorios/AgenteRepository.java
@@ -2,6 +2,8 @@ package com.seminario.gestorInmobiliario.Repositorios;
 
 import com.seminario.gestorInmobiliario.Entidades.Agente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AgenteRepository extends JpaRepository<Agente, String> {
 }
